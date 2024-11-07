c1e62e359406ed908a955e79002cfa95fb43c09e
JunhaPark96 <njk116@gmail.com>
2024-11-07 21:08:43 +0900

    final3

--- lib/admin_list/feature/page8_Calendar/view_models/event_provider.dart
+++ lib/admin_list/feature/page8_Calendar/view_models/event_provider.dart
@@ -8,9 +8,9 @@ class EventProvider with ChangeNotifier {
         id: 1,
         title: 'repcap01 서버 백신 패치 업데이트 작업',
         description: '14시부터 16시까지 repcap01 서버 백신 패치 업데이트 작업',
-        startTime: DateTime(DateTime.now().year, DateTime.now().month,
+        start_time: DateTime(DateTime.now().year, DateTime.now().month,
             DateTime.now().day, 14, 0),
-        endTime: DateTime(DateTime.now().year, DateTime.now().month,
+        end_time: DateTime(DateTime.now().year, DateTime.now().month,
             DateTime.now().day, 16, 0),
         type: 'maintenance',
       ),
@@ -18,9 +18,9 @@ class EventProvider with ChangeNotifier {
         id: 2,
         title: 'pepeap 01 서버 백신 패치 업데이트 작업',
         description: '16시부터 17시까지 repcap01 서버 백신 패치 업데이트 작업',
-        startTime: DateTime(DateTime.now().year, DateTime.now().month,
+        start_time: DateTime(DateTime.now().year, DateTime.now().month,
             DateTime.now().day, 16, 0),
-        endTime: DateTime(DateTime.now().year, DateTime.now().month,
+        end_time: DateTime(DateTime.now().year, DateTime.now().month,
             DateTime.now().day, 17, 0),
         type: 'maintenance',
       ),
@@ -34,15 +34,15 @@ class EventProvider with ChangeNotifier {
 
   List<Event> getAllEvents() {
     List<Event> allEvents = [];
-    _events.values.forEach((events) {
+    for (var events in _events.values) {
       allEvents.addAll(events);
-    });
+    }
     return allEvents;
   }
 
   void addEvent(Event event) {
     final date = DateTime(
-        event.startTime.year, event.startTime.month, event.startTime.day);
+        event.start_time.year, event.start_time.month, event.start_time.day);
     if (_events[date] != null) {
       _events[date]!.add(event);
     } else {
@@ -53,15 +53,15 @@ class EventProvider with ChangeNotifier {
 
   void removeEvent(Event event) {
     final date = DateTime(
-        event.startTime.year, event.startTime.month, event.startTime.day);
+        event.start_time.year, event.start_time.month, event.start_time.day);
     _events[date]?.removeWhere(
-        (e) => e.title == event.title && e.startTime == event.startTime);
+        (e) => e.title == event.title && e.start_time == event.start_time);
     notifyListeners();
   }
 
   void updateEvent(Event updatedEvent) {
-    final date = DateTime(updatedEvent.startTime.year,
-        updatedEvent.startTime.month, updatedEvent.startTime.day);
+    final date = DateTime(updatedEvent.start_time.year,
+        updatedEvent.start_time.month, updatedEvent.start_time.day);
     final index =
         _events[date]?.indexWhere((event) => event.id == updatedEvent.id);
     if (index != null && index != -1) {
